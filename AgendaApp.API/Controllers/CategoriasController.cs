bf42de35fe834efd26e15d7272fbb2d579083546
leoluciocb1-full-stack <leoluciocb1@gmail.com>
2026-08-04 19:28:34 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaApp.API/Controllers/CategoriasController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        //Serviço para consulta de categorias
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ AgendaApp.API/Controllers/TarefasController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        //Serviço para cadastro de tarefas
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        //Serviço para atualização de tarefas
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        //Serviço para exclusão de tarefas
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+    }
+}
