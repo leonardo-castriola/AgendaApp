34b1d93c05be7432cee0cd6f129fb326afcfe2b1
leoluciocb1-full-stack <leoluciocb1@gmail.com>
2026-08-13 19:01:08 -0300

    Adiciona a documentação do Scalar e a configuração do CORS.

--- AgendaApp.API/Program.cs
+++ AgendaApp.API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -6,6 +8,14 @@ builder.Services.AddOpenApi();
 //Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Criando a política de Cors
+builder.Services.AddCors(c => c.AddPolicy("DefaultPolicy", builder =>
+{
+    builder.WithOrigins("http://localhost:4200")
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+}));
 
 var app = builder.Build();
 
@@ -17,6 +27,12 @@ if (app.Environment.IsDevelopment())
 //Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
+
+//Scalar
+app.MapScalarApiReference(s => s.WithTheme(ScalarTheme.BluePlanet));
+
+//Executando a política de Cors
+app.UseCors("DefaultPolicy");
 
 app.UseAuthorization();
 
